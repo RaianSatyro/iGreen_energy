915b5c5ded51356bce6e0dd727298fe1a3f697b8
Raian Satyro <raiansatyro45@gmail.com>
2023-09-28 12:28:47 -0300

    [Melhoria] Foi feito uma melhoria no codigo JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 let botaoWhatsApp = document.getElementById("whatsapp")
-
+let numeroWhatsapp = 5524999349684
+let textoEnviado = 'Testando'
 botaoWhatsApp.addEventListener("click", function() {
-  let whatsapplink = "https://api.whatsapp.com/send?phone=5524999349684&text=TESTE"; 
+  let whatsapplink = `https://api.whatsapp.com/send?phone=${numeroWhatsapp}&text=${textoEnviado}`; 
   // Use window.location.href para redirecionar para o novo URL
   window.open(whatsapplink, "_blank")
 });
