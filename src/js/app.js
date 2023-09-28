dd81acfcf1794ce5340d8bda7107c9a636128c9d
Raian Satyro <raiansatyro45@gmail.com>
2023-09-28 17:04:46 -0300

    [Dev] Desenvolvido mais um sessão da pagina, e iniciado uma nova.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
-let botaoWhatsApp = document.getElementById("whatsapp")
-let numeroWhatsapp = 5524999349684
-let textoEnviado = 'Testando'
-botaoWhatsApp.addEventListener("click", function() {
+let botaoWhatsApp = document.getElementById("whatsapp")//Pega o elemento botão no HTML pelo ID
+let numeroWhatsapp = 552499999999//SEU NUMERO AQUI COM CODIGO PAIS E DDD
+let textoEnviado = 'SUA MENSAGEM AQUI'
+
+
+botaoWhatsApp.addEventListener("click", function() { //CRIA E EXECULTA A FUNCAO QUANDO O BOTAO FOR CLICADO
   let whatsapplink = `https://api.whatsapp.com/send?phone=${numeroWhatsapp}&text=${textoEnviado}`; 
-  // Use window.location.href para redirecionar para o novo URL
+  // Use window.open(LINK, AÇÃO) para redirecionar para o novo URL EM UMA NOVA ABA
   window.open(whatsapplink, "_blank")
 });
